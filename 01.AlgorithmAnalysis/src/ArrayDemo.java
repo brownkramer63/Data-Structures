acf48d26884ed91efb4901cb67f1be4bc15ebaaa
Kramer Brown <brownkramer63@gmail.com>
2022-08-28 14:50:32 -0500

    push

---
+++ 01.AlgorithmAnalysis/src/ArrayDemo.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int [] scorArray= new int[10];
+
+        System.out.println(Arrays.toString(scorArray));
+
+
+
+
+
+
+    }
+}
