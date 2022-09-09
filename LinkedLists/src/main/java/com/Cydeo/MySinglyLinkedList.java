e4460739c0060831a75d2b27cc6b5c42d3f1d164
Kramer Brown <brownkramer63@gmail.com>
2022-09-09 12:09:43 -0500

    linkedlists

---
+++ LinkedLists/src/main/java/com/Cydeo/MySinglyLinkedList.java
@@ -0,0 +1,36 @@
+package com.Cydeo;
+
+public class MySinglyLinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    void add(int data) {
+        // create a new node object from data
+        Node node = new Node(data);
+
+        if (isEmpty()) { // if the list is empty
+            head = tail = node;
+            size++;
+        } else { // if there are elements in list
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+
+    }
+    void printNodes(){
+        Node current=head;
+        while(current!=null){
+            if (current.next==null) System.out.println(current.id+"=> null");
+            else {
+                System.out.print(current.id+"=> ");
+            }
+            current=current.next;
+        }
+    }
+}
